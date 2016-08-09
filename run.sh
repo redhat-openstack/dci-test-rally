baf474badc39ac879ae3702f6029aa93189deb8a
Gonéri Le Bouder <goneri@redhat.com>
2016-08-09 15:41:01 +0200

    glance image-create: drop the --progress
    Avoid glance progress bare. This to reduce the amount of log.

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ if [ ! -f CentOS-7-x86_64-GenericCloud.raw ]; then
     find . -regex '\./CentOS-7-x86_64-GenericCloud-*[0-9]+.raw' -exec mv {} CentOS-7-x86_64-GenericCloud.raw \;
 fi
 
-glance image-show CentOS-7-x86_64-GenericCloud || glance image-create --name "CentOS-7-x86_64-GenericCloud" --disk-format qcow2 --container-format bare --is-public=1 --progress < CentOS-7-x86_64-GenericCloud.raw
+glance image-show CentOS-7-x86_64-GenericCloud || glance image-create --name "CentOS-7-x86_64-GenericCloud" --disk-format qcow2 --container-format bare --is-public=1 < CentOS-7-x86_64-GenericCloud.raw
 
 [ -f cirros-0.3.2-x86_64-vmlinuz ] || curl http://download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-uec.tar.gz  | tar zfxv -
 KERNEL_ID=`glance image-create --name "cirros-0.3.2-x86_64-uec-kernel" --disk-format aki --container-format aki --is-public=1 --file cirros-0.3.2-x86_64-vmlinuz | awk '/ id / { print $4 }'`
